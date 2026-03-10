f2861cf3c99318b9bb8191ab7ca0d90dd4ca792d
wdm-coding <wdmcoding0620@163.com>
2026-03-10 11:39:12 +0800

    koa重构项目

--- mulu/koa/router/index.js
+++ mulu/koa/router/index.js
@@ -1,11 +1,9 @@
 const Router = require('@koa/router');
-const router = new Router({prefix: '/api'}); // 创建路由中间件
+const router = new Router({
+    prefix: '/api'
+})
 // 定义路由规则
 router.get('/', ctx => {
     ctx.body = '首页';
 })
-router.get('/user', ctx => {
-    ctx.body = '用户';
-})
-
 module.exports = router;
